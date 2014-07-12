270c68a5f7fcb41b0086d79dcf9037a9e76650f7
Michael Perez <michaelp193@gmail.com>
2014-07-12 16:35:58 -0400

    after years

--- compileData.py
+++ compileData.py
@@ -20,15 +20,21 @@ def extract_by(key, value, arr_of_obj, extra_value = None):
             #print obj
             result.append(obj)
     return result
+def assignDate(arr_of_obj):
+    for obj in arr_of_obj:
+        if obj["CALENDARIO"]:
+            obj["year"] = obj["CALENDARIO"][-5:]
+
 def count_by(key, arr_of_obj):
     result = {}
     for obj in arr_of_obj:
         category = obj[key].lower()
-        if gender not in result:
+        if category not in result:
             result[category] = 1
         else:
             result[category] += 1
     return result
+
 def mean (arr):
     total = 0
     for num in arr:
@@ -42,6 +48,7 @@ def calc_objs_avg(key, arr_of_obj):
     return round(mean(arr),2)
 
 def print_stats(total, subset):
+    assignDate(subset)
     total_length = len(total)
     subset_length = len(subset)
     perc_subset = round(float(subset_length) / total_length * 100, 2)
@@ -49,12 +56,16 @@ def print_stats(total, subset):
     perc_male = round(float(genders["masculino"]) / subset_length * 100, 2)
     perc_female = round(float(genders["femenino"]) / subset_length * 100, 2)
     avg_gpa =  calc_objs_avg("GPA", subset)
+    years = count_by("year", subset)
     print "Total students: %d" % (total_length)
     print "Total Computer Science students: %d" % (subset_length)
     print "Percentage of Computer Science students:", perc_subset, "%"
     print "Percentage of Male Computer Science students:", perc_male, "%"
     print "Percentage Female Computer Science students: ", perc_female, "%"
     print "Average GPA:", avg_gpa
+    print "Admissions by Year:"
+    for tup in years.items():
+        print tup[0], tup[1]
 
 
 def main ():
